dfc850a9419ea7da6f6c176092c35997fe30e056
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-13 12:46:26 -0700

    Create addtwonumbers.cpp

---
+++ addtwonumbers.cpp
@@ -0,0 +1,14 @@
+// Kattis-Cpp-Solutions
+// Coded by Daniyal arteshdar
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b ;
+    cin >> a >> b ;
+    cout << a + b ;
+    return 0 ;
+}
