6fbcfce2f69726e67fc131e52f92030419ccba79
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-13 12:49:21 -0700

    Create areal.cpp

---
+++ areal.cpp
@@ -0,0 +1,12 @@
+// Kattis-Cpp-Solutions
+// Coded by Daniyal Arteshdar 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long a;
+  cin >> a;
+  cout << fixed << setprecision(7);
+  cout << 4 * sqrt(a) << endl;
+  return 0;
+}
