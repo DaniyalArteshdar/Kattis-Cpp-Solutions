41840c2b5364b4cebc8f414f42a4aeeb1ac44dd3
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-13 12:50:35 -0700

    Create ladder.cpp

---
+++ ladder.cpp
@@ -0,0 +1,12 @@
+// Kattis-Cpp-Solutions
+// Coded by Daniyal Arteshdar
+#include <bits/stdc++.h>
+#define PI acos(-1.0)
+using namespace std;
+
+int main() {
+  int h, v;
+  cin >> h >> v;
+  cout << ceil(h / sin(v * PI / 180)) << endl;
+  return 0;
+}
