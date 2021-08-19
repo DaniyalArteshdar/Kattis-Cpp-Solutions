42911bded5f36fd7572c61d60e9129c7c214fb34
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-18 22:46:00 -0700

    Create fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,36 @@
+// Kattis-Cpp-Solutions
+// Coded by Daniyal Arteshdar
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+
+{
+    int x, y, n ;
+    cin >> x >> y >> n ;
+
+    for (int i = 1 ; i < n+1 ; i++)
+    {
+
+            if ((i%x == 0) && (i%y == 0))
+            {
+                cout << "FizzBuzz" << endl ;
+            }
+            else if (i % x == 0)
+            {
+                cout << "Fizz" << endl ;
+            }
+            else if (i % y == 0)
+            {
+                cout << "Buzz" << endl ;
+            }
+            else
+            {
+                cout << i <<endl ;
+            }
+    }
+    return 0;
+}
