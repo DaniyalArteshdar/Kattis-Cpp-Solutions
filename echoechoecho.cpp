fbc9c92dea845d401fdfec6d1f4d8e5bd0a118c3
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-26 10:14:58 -0700

    Create echoechoecho.cpp

---
+++ echoechoecho.cpp
@@ -0,0 +1,20 @@
+// Kattis-Cpp-Solutions
+// Coded by Daniyal Arteshdar
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    string inp ;
+    cin >> inp ;
+
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        cout << inp << " ";
+    }
+
+    return 0;
+}
